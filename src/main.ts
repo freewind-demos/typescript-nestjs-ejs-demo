89a5afaba9a6ef04420745bbdd8f521b52d1f37d
Freewind <nowindlee@gmail.com>
2024-01-30 21:56:20 +0800

    find-port

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import portfinder from 'portfinder';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
-  console.log("open http://localhost:3000")
+  await app.listen(await portfinder.getPortPromise());
+  console.log(await app.getUrl())
 }
 bootstrap();
